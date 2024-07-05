7026b8ef60848c80f8706a6a8c9138e86fd660a8
Himakar Raju <64581661+HimakarRaju@users.noreply.github.com>
2024-07-05 20:09:17 +0530

    Update main.py

--- 28June/Assignments/bankingSystemType2/main.py
+++ 28June/Assignments/bankingSystemType2/main.py
@@ -1,5 +1,22 @@
 import os
 from register import register
 from login import login
+from utils import initialize_file
 
-accounts_file = "accounts.txt"
+def main():
+    initialize_file()
+    
+    while True:
+        print("1. Login")
+        print("2. Register")
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            login()
+        elif choice == "2":
+            register()
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
